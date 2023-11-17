56b394af1b4c82e3c19e438e795bf2a91d433fff
lees846 <lees846@newschool.edu>
2023-11-17 14:36:07 -0500

    initialize scene files and switch case structure

---
+++ scenes/scene1.js
@@ -0,0 +1,5 @@
+function scene1() {
+    background("skyblue");
+    fill("white");
+    text("Scene 1: Instruction Screen", width/2, height/2);
+}
---
+++ scenes/scene3.js
@@ -0,0 +1,5 @@
+function scene3() {
+    background("skyblue");
+    fill("white");
+    text("Scene 3: Measurement Minigame", width/2, height/2);
+}
