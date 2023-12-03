c657635279a6446a3076d89891f46402c4883ec9
lees846 <lees846@newschool.edu>
2023-12-03 14:30:10 -0500

    change file and function names for new switch case structure

--- scenes/scene4.js
+++ scenes/scene4.js
@@ -1,5 +1,5 @@
 function scene4() {
     background("skyblue");
     fill("white");
-    text("Scene 4: QG Abandoned, Lose + Retry Screen", width/2, height/2);
+    text("Scene 4: Quantum Swarm, Win + Retry Screen", width/2, height/2);
 }
